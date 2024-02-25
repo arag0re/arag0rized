ea6dd8c970b8a84a3b6d1727c221b3dd3e7a5144
arag0re.eth <44698885+arag0re@users.noreply.github.com>
2024-02-25 22:17:19 +0100

    Update arag0rize.sh

--- arag0rize.sh
+++ arag0rize.sh
@@ -5,10 +5,10 @@ apt install -y curl wget git zsh ca-certificates nano
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 for user in /home/*; do
   if [ -d "$user" ]; then
-    chown "$user":"$user" "/home/$user/.zshrc"
-    git clone https://github.com/zsh-users/zsh-autosuggestions "/home/$user/.oh-my-zsh/custom/plugins/zsh-autosuggestions"; \
-    sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' "/home/$user/.zshrc"
-    chsh -s /bin/zsh "$user"
+    username=$(basename "$user")
+    chown "$username":"$username" "$user/.zshrc"
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$user/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+    sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' "$user/.zshrc"
+    chsh -s /bin/zsh "$username"
   fi
 done
-source "$USER/.zshrc"
